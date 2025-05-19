079a6cbb61fdffc2c9328153325968bbd53f8c65
Lucas Torres <lucastorreslr10@gmail.com>
2025-05-19 00:21:43 -0300

    feat: add general styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
-      <div>React App</div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const footerStyle = {
+  footer: "",
+};
+
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+export default Footer;
