bac68420a4632d4c95fa16aa7525c8cae3ae8f3b
Lucas Torres <lucastorreslr10@gmail.com>
2025-05-22 02:06:07 -0300

    feat: add feed infinite scroll

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,16 +1,54 @@
 import React from "react";
 import FeedModal from "./FeedModal";
 import FeedPhotos from "./FeedPhotos";
+import { useOutletContext } from "react-router-dom";
 
 const Feed = () => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
+  const [pages, setPages] = React.useState([1]);
+  const [infinite, setInfinite] = React.useState(true);
+  const { user } = useOutletContext() || {};
+
+  React.useEffect(() => {
+    let wait = false;
+    function infiniteScroll() {
+      if (infinite) {
+        const scroll = window.scrollY;
+        const height = document.body.offsetHeight - innerHeight;
+
+        if (scroll > height * 0.75 && !wait) {
+          setPages((pages) => [...pages, pages.length + 1]);
+          wait = true;
+          setTimeout(() => {
+            wait = false;
+          }, 500);
+        }
+      }
+    }
+
+    window.addEventListener("wheel", infiniteScroll);
+    window.addEventListener("scroll", infiniteScroll);
+
+    return () => {
+      window.removeEventListener("wheel", infiniteScroll);
+      window.removeEventListener("scroll", infiniteScroll);
+    };
+  }, [infinite]);
 
   return (
     <div>
       {modalPhoto && (
         <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
       )}
-      <FeedPhotos setModalPhoto={setModalPhoto} />
+      {pages.map((page) => (
+        <FeedPhotos
+          key={page}
+          page={page}
+          user={user}
+          setModalPhoto={setModalPhoto}
+          setInfinite={setInfinite}
+        />
+      ))}
     </div>
   );
 };
--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import UserHeader from "./UserHeader";
 import { Outlet } from "react-router-dom";
+import { UserContext } from "../../Contexts/UserContext";
 
 const User = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <section className={`container`}>
       <UserHeader />
-      <Outlet />
+      <Outlet context={{ user: data.id }} />
     </section>
   );
 };
